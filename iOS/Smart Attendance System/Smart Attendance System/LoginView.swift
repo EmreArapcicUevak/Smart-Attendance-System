790e4af2fc21e13cc8a6ce6a423da52012bc737e
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-05 01:15:42 +0200

    Start work on the iOS application

---
+++ iOS/Smart Attendance System/Smart Attendance System/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Smart Attendance System
+//
+//  Created by Emre Arapcic-Uevak on 5. 5. 2025..
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ iOS/Smart Attendance System/Smart Attendance SystemTests/Smart_Attendance_SystemTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_Attendance_SystemTests.swift
+//  Smart Attendance SystemTests
+//
+//  Created by Emre Arapcic-Uevak on 5. 5. 2025..
+//
+
+import Testing
+@testable import Smart_Attendance_System
+
+struct Smart_Attendance_SystemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
