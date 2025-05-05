4779739475e964847ebf49c57b0f9898132e7fcb
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-05 02:03:15 +0200

    Add the color pallet

--- iOS/Smart Attendance System/Smart Attendance System/Views/LoginView.swift
+++ iOS/Smart Attendance System/Smart Attendance System/Views/LoginView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color
+                .surface
+                .ignoresSafeArea()
         }
-        .padding()
     }
 }
 
