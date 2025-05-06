aa768d5ff46e0d6a09b05d8cc58e8173e618b35e
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-06 20:37:51 +0200

    Start work on the detailed course view

---
+++ iOS/Smart Attendance System/Smart Attendance System/Models/StudentModel.swift
@@ -0,0 +1,18 @@
+//
+//  StudentModel.swift
+//  Smart Attendance System
+//
+//  Created by Emre Arapcic-Uevak on 6. 5. 2025..
+//
+
+import Foundation
+
+struct StudentModel : Hashable, Identifiable {
+    var major: String
+    var firstName: String
+    var secondName: String
+    var email: String
+    var student_id: String
+    
+    var id: String { student_id }
+}
---
+++ iOS/Smart Attendance System/Smart Attendance System/Views/DetailedCourseView/DetailedCourseView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailedCourseView.swift
+//  Smart Attendance System
+//
+//  Created by Emre Arapcic-Uevak on 6. 5. 2025..
+//
+
+import SwiftUI
+
+struct DetailedCourseView: View {
+    var body: some View {
+        ZStack {
+            Color.surface
+                .ignoresSafeArea()
+            
+            
+        }
+    }
+}
+
+#Preview {
+    DetailedCourseView()
+}
