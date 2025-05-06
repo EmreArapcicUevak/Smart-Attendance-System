738de794209d5e227d933fe392b213665e1b8d47
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-06 23:48:38 +0200

    Add 2 view models one for all the add button functions and another for the student filter function which will be used later on

---
+++ iOS/Smart Attendance System/Smart Attendance System/ViewModels/AddButtonFuncs.swift
@@ -0,0 +1,14 @@
+//
+//  AddDashboardFunc.swift
+//  Smart Attendance System
+//
+//  Created by Emre Arapcic-Uevak on 6. 5. 2025..
+//
+
+import Foundation
+import SwiftUI
+
+func addCreateCourseView(path: Binding<NavigationPath>) {
+    print("Finish adding course")
+    //path.wrappedValue.append(UserModel()) // Change later!!!
+}
---
+++ iOS/Smart Attendance System/Smart Attendance System/ViewModels/StudentFilter.swift
@@ -0,0 +1,15 @@
+//
+//  StudentFilter.swift
+//  Smart Attendance System
+//
+//  Created by Emre Arapcic-Uevak on 6. 5. 2025..
+//
+
+import Foundation
+
+func studentFilter(_ students: [StudentModel], _ filter: String) -> [StudentModel] {
+    let trimmedFilter = filter.trimmingCharacters(in: .whitespaces).lowercased()
+    return students.filter { student in
+        return filter == "" || student.name.lowercased().contains(trimmedFilter) || student.student_id.contains(trimmedFilter)
+    }
+}
