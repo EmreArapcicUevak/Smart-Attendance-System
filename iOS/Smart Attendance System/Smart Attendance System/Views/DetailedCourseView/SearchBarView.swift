b1fa68ca9b8f63516f9e6ec1ae034c2cdf921fa1
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-06 20:57:24 +0200

    Add SearchBarView file

---
+++ iOS/Smart Attendance System/Smart Attendance System/Views/DetailedCourseView/SearchBarView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  Smart Attendance System
+//
+//  Created by Emre Arapcic-Uevak on 6. 5. 2025..
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarView()
+}
