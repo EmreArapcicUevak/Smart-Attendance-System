0284b896761e62cdeda7d75e8cbe4c7fd25f4c60
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-06 23:52:07 +0200

    Finish work on the current version of the detailed course view Will update again when the backend is ready

--- iOS/Smart Attendance System/Smart Attendance System/Views/DetailedCourseView/SearchBarView.swift
+++ iOS/Smart Attendance System/Smart Attendance System/Views/DetailedCourseView/SearchBarView.swift
@@ -8,11 +8,46 @@
 import SwiftUI
 
 struct SearchBarView: View {
+    @Binding var searchBarText: String
+    var placeholder: String
+    let outlineColor: Color
+    let fillColor: Color
+    let cornerRadius: CGFloat
+    let lineWidth: CGFloat
+    
+    init(
+        searchBarText: Binding<String>,
+        placeholder: String,
+        outlineColor: Color = Color.outline,
+        fillColor: Color = Color.surfContainer,
+        cornerRadius: CGFloat = 5,
+        lineWidth: CGFloat = 1
+    ) {
+        self._searchBarText = searchBarText
+        self.placeholder = placeholder
+        self.outlineColor = outlineColor
+        self.fillColor = fillColor
+        self.cornerRadius = cornerRadius
+        self.lineWidth = lineWidth
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            TextField(placeholder, text: $searchBarText)
+            Image(systemName: "magnifyingglass")
+        }
+        .padding()
+        .background(
+            RoundedRectangleWithOutlineView(
+                outlineColor: outlineColor,
+                fillColor: fillColor,
+                cornerRadius: cornerRadius,
+                lineWidth: lineWidth
+            )
+        )
     }
 }
 
 #Preview {
-    SearchBarView()
+    SearchBarView(searchBarText: .constant(""), placeholder: "Filter by name")
+        .padding()
 }
---
+++ iOS/Smart Attendance System/Smart Attendance System/Views/FloatingViews/FloatingCameraButtonView.swift
@@ -0,0 +1,36 @@
+//
+//  FloatingButton.swift
+//  Smart Attendance System
+//
+//  Created by Emre Arapcic-Uevak on 6. 5. 2025..
+//
+
+import SwiftUI
+
+struct FloatingCameraButtonView: View {
+    @Binding var path: NavigationPath
+    var body: some View {
+        Button {
+            path.append(OpenCameraEnum.openCameraView)
+        } label: {
+            Image(systemName: "camera.fill")
+                .foregroundStyle(Color.myPrimary)
+                .font(.system(size: 20))
+                .background(
+                    RoundedRectangleWithOutlineView(
+                        fillColor: Color.surfContainerHighest,
+                        cornerRadius: 12
+                    )
+                    .frame(width: 56, height: 56)
+                )
+                
+        }
+
+    }
+}
+
+#Preview {
+    @Previewable @State var path = NavigationPath()
+    
+    FloatingCameraButtonView(path: $path)
+}
