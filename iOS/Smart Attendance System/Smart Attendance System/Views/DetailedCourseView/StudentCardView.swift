da240b841cb0aff3145dc3e7422c5617d3a4963b
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-06 20:58:23 +0200

    Finish the current version of the student card...
    Waiting for the backend to send me the rest of the data

--- iOS/Smart Attendance System/Smart Attendance System/Views/DetailedCourseView/StudentCardView.swift
+++ iOS/Smart Attendance System/Smart Attendance System/Views/DetailedCourseView/StudentCardView.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct StudentCardView: View {
+    var student: StudentModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                // Name and ID
+                VStack(alignment: .leading) {
+                    Text(student.name)
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(Color.myPrimary)
+                    
+                    Text(student.student_id)
+                        .font(.subheadline)
+                        .foregroundStyle(Color.mySecondary)
+                }
+                
+                Spacer()
+                
+                Image(systemName: "person.crop.square")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: 40)
+                    .foregroundStyle(Color.mySecondary)
+                    
+            }
+            
+            Divider()
+                .padding(.vertical)
+                .foregroundStyle(Color.mySecondary)
+            
+            // Add attendance here!
+        }
+        .padding()
+        .background(
+            RoundedRectangleWithOutlineView(fillColor: Color.surfContainer)
+        )
     }
 }
 
 #Preview {
-    StudentCardView()
+    StudentCardView(
+        student: .init(
+            major: "CSE",
+            firstName: "Emre",
+            secondName: "Arapcic-Uevak",
+            email: "EmreArapcicUevak@gmail.com",
+            student_id: "220302289"
+        )
+    )
+    .padding()
 }
