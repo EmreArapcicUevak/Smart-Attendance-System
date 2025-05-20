d3867b3c943a254537a8417c07b25292473928ce
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-20 18:42:51 +0200

    Start work on the email validation for the login view and also start writing tests

---
+++ iOS/Smart Attendance System/Login View Controller Test/Login_View_Controller_Test.swift
@@ -0,0 +1,16 @@
+//
+//  Login_View_Controller_Test.swift
+//  Login View Controller Test
+//
+//  Created by Emre Arapcic-Uevak on 20. 5. 2025..
+//
+
+import Testing
+
+struct Login_View_Controller_Test {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ iOS/Smart Attendance System/Smart Attendance System/ViewModels/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  Smart Attendance System
+//
+//  Created by Emre Arapcic-Uevak on 20. 5. 2025..
+//
+
+import Foundation
