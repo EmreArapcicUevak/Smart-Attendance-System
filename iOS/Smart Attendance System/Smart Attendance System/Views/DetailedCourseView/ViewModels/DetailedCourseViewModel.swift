f05717166575fed53db1800e45e5420401249961
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-22 12:09:28 +0200

    Start work on the detailed course view model

---
+++ iOS/Smart Attendance System/Smart Attendance System/Views/DetailedCourseView/ViewModels/DetailedCourseViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  DetailedCourseViewModel.swift
+//  Smart Attendance System
+//
+//  Created by Emre Arapcic-Uevak on 22. 5. 2025..
+//
+
+import Foundation
+import SwiftUI
+
+@Observable
+class DetailedCourseViewModel {
+    var studnets : [StudentModel] = []
+    var isLoading : Bool = true
+    
+}
