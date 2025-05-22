cac1a35f66b2873d59d825cb1bf18aed867a4b9e
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-22 14:06:54 +0200

    Add a new struct for constants

---
+++ iOS/Smart Attendance System/Smart Attendance System/Models/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smart Attendance System
+//
+//  Created by Emre Arapcic-Uevak on 22. 5. 2025..
+//
+
+import Foundation
+
+struct Constants {
+    static let  API_URL = "http://127.0.0.1:8080"
+}
