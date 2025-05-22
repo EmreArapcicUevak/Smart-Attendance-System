7f91d9e0d667873db1c7e70668ad07d649b43854
VedadSiljic <siljicvedad17@gmail.com>
2025-05-22 20:52:53 +0200

    Change theme and add logo to the login screen

--- Android/app/src/main/java/com/example/smartattendancesystemandroid/ui/screens/login/LoginScreen.kt
+++ Android/app/src/main/java/com/example/smartattendancesystemandroid/ui/screens/login/LoginScreen.kt
@@ -2,18 +2,21 @@ package com.example.smartattendancesystemandroid.ui.screens.login
 
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -26,12 +29,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.smartattendancesystemandroid.R
 import com.example.smartattendancesystemandroid.auth.AuthResult
 import com.example.smartattendancesystemandroid.ui.components.LoadingCircleScreen
 import com.example.smartattendancesystemandroid.ui.theme.SmartAttendanceSystemAndroidTheme
@@ -96,12 +101,21 @@ private fun LoginScreenContent(
 ) {
     Scaffold { innerPadding ->
         Column(
-            verticalArrangement = Arrangement.Center,
             modifier = Modifier
                 .padding(innerPadding)
                 .padding(8.dp)
                 .fillMaxSize()
         ) {
+
+            Image(
+                painter = painterResource(R.drawable.smartattendancesystenmlogo),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(250.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
 
             Text(
                 text = "Login",
--- Android/app/src/main/java/com/example/smartattendancesystemandroid/ui/theme/Type.kt
+++ Android/app/src/main/java/com/example/smartattendancesystemandroid/ui/theme/Type.kt
@@ -2,12 +2,15 @@ package com.example.smartattendancesystemandroid.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.smartattendancesystemandroid.R
+
 
 // Set of Material typography styles to start with
-val Typography = Typography(
+val baseline = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
@@ -31,4 +34,30 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val bodyFontFamily = FontFamily(
+    Font(R.font.inter)
+)
+
+val displayFontFamily = FontFamily(
+    Font(R.font.poppins)
+)
+
+val Typography = Typography(
+    displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
+    displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
+    displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = displayFontFamily),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = displayFontFamily),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = displayFontFamily),
+    titleLarge = baseline.titleLarge.copy(fontFamily = displayFontFamily),
+    titleMedium = baseline.titleMedium.copy(fontFamily = displayFontFamily),
+    titleSmall = baseline.titleSmall.copy(fontFamily = displayFontFamily),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = bodyFontFamily),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = bodyFontFamily),
+    bodySmall = baseline.bodySmall.copy(fontFamily = bodyFontFamily),
+    labelLarge = baseline.labelLarge.copy(fontFamily = bodyFontFamily),
+    labelMedium = baseline.labelMedium.copy(fontFamily = bodyFontFamily),
+    labelSmall = baseline.labelSmall.copy(fontFamily = bodyFontFamily),
 )
