f47c70a9f7b32afbf5ae6f3de8c085710844b264
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-23 19:03:37 +0200

    Start work on the course information view

--- iOS/Smart Attendance System/Smart Attendance System/Views/CourseInformationView/CourseInformationView.swift
+++ iOS/Smart Attendance System/Smart Attendance System/Views/CourseInformationView/CourseInformationView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct CourseInformationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color
+                .surface
+                .ignoresSafeArea()
+            
+            
+        }
     }
 }
 
