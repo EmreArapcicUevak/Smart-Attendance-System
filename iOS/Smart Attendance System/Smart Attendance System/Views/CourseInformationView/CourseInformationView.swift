0440dc4b34988e33ab257a1d9091ec41845b02b5
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-05-23 18:25:09 +0200

    Start work on the CourseInformationView

---
+++ iOS/Smart Attendance System/Smart Attendance System/Views/CourseInformationView/CourseInformationView.swift
@@ -0,0 +1,18 @@
+//
+//  CourseInformationView.swift
+//  Smart Attendance System
+//
+//  Created by Emre Arapcic-Uevak on 23. 5. 2025..
+//
+
+import SwiftUI
+
+struct CourseInformationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CourseInformationView()
+}
